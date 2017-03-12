f1147f9f6873dece16eb73a4a28405bde4e5fb9e
José Oliveros <jose.oliveros.1983@gmail.com>
2017-03-11 20:27:13 -0600

    Update example-1 to have logging and subscribe to a channel

--- examples/example-1.py
+++ examples/example-1.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import
 from bitmex_websocket.websocket import BitMEXWebsocket
 from time import sleep
+import logging
+import websocket
+
+_logger = logging.getLogger('websocket')
+_logger.setLevel(logging.DEBUG)
+websocket.enableTrace(True)
 
 ws = BitMEXWebsocket()
 ws.connect()
+ws.subscribe('instrument')
 
 while True:
     sleep(1)
