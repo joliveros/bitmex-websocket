56e63bfbc2cc33748595064694bf8f80e7340fd6
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-06-02 15:38:00 -0500

    restore reading of requirements into setup
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
+
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from pip.req import parse_requirements
+from pip.download import PipSession
 from os.path import realpath
+
+
+def get_reqs_from_file(file):
+    file_path = realpath(file)
+
+    # parse_requirements() returns generator of pip.req.InstallRequirement objects
+    install_reqs = parse_requirements(file_path, session=PipSession)
+
+    # reqs is a list of requirement
+    # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+    return [str(ir.req) for ir in install_reqs]
 
 
 def get_version_info():
@@ -19,7 +33,9 @@ setup(name='bitmex_websocket',
       url='https://github.com/joliveros/bitmex-websocket',
       packages=['bitmex_websocket',
                 'bitmex_websocket.auth'],
+      install_requires=get_reqs_from_file('./requirements.txt'),
       include_package_data=True,
+      tests_require=get_reqs_from_file('./requirements-test.txt'),
       license='MIT License',
       zip_safe=True,
       keywords='bitmex websocket bot cryptocurrency',
@@ -28,9 +44,5 @@ setup(name='bitmex_websocket',
         'Intended Audience :: Developers',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
       ])
